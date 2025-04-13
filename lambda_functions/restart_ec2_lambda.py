ec6fbd734f1bccc7dcc7b9c6f1acbfcb621378d2
kottarainen <maria@LAPTOP-KETUCH53>
2025-04-13 13:29:31 +0300

    added dynamodb logging for lambda1

--- lambda_functions/restart_ec2_lambda.py
+++ lambda_functions/restart_ec2_lambda.py
@@ -1,7 +1,30 @@
 import boto3
 import time
+import uuid
+import os
+from datetime import datetime
 
 ec2_client = boto3.client("ec2", region_name="eu-central-1")
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(os.environ["AUDIT_LOG_TABLE"])
+
+def log_audit_event(instance_id, status, error=None):
+    try:
+        table.put_item(Item={
+            'incidentId': str(uuid.uuid4()),
+            'useCase': 'HighCPU',
+            'resourceId': instance_id,
+            'timestamp': datetime.utcnow().isoformat(),
+            'actionTaken': 'Restart EC2',
+            'status': status,
+            'details': {
+                'region': 'eu-central-1'
+            },
+            'errorMessage': error or ""
+        })
+        print("Audit event logged.")
+    except Exception as e:
+        print(f"Failed to log audit event: {str(e)}")
 
 def lambda_handler(event, context):
     instance_id = event["detail"]["instance-id"]
@@ -22,11 +45,13 @@ def lambda_handler(event, context):
             if state == "stopped":
                 print(f"Instance {instance_id} has stopped.")
                 break
-            time.sleep(5)  # wait 5 sec before checking again
+            time.sleep(5)
 
         # start the instance
         ec2_client.start_instances(InstanceIds=[instance_id])
         print(f"Instance {instance_id} started successfully.")
+
+        log_audit_event(instance_id, "Success")
 
         return {
             "statusCode": 200,
@@ -35,6 +60,8 @@ def lambda_handler(event, context):
 
     except Exception as e:
         print(f"Error restarting instance: {str(e)}")
+        log_audit_event(instance_id, "Failure", str(e))
+
         return {
             "statusCode": 500,
             "body": f"Error restarting instance: {str(e)}"
