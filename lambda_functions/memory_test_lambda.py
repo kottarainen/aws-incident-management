3c4cf3afce8c213eca10692131cd1f0e485fdc45
kottarainen <maria@LAPTOP-KETUCH53>
2025-04-24 17:48:50 +0300

    updated lambda code for testing

--- lambda_functions/memory_test_lambda.py
+++ lambda_functions/memory_test_lambda.py
@@ -1,7 +1,3 @@
 def lambda_handler(event, context):
-    # Simulate some work to use memory (not dangerous)
-    big_list = [i for i in range(1000000)]  # Just for testing
-    return {
-        'statusCode': 200,
-        'body': f"Allocated list of size {len(big_list)}"
-    }
+    a = ['a'] * (10**8)  # Force memory error
+    return { 'statusCode': 200 }
